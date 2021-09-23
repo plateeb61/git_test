1281fa270e2e39b5ea652a42517e2430c5aacdd2
plateeb61 <84173786+plateeb61@users.noreply.github.com>
2021-09-23 23:44:38 +0900

    Create pi_method.py

---
+++ pi_method.py
@@ -0,0 +1,14 @@
+
+#horizontal distance from car to obstacles by angle
+
+def raw2horiDist(distanceList, rawDistList, srvo_ang, angleList):
+    for i in range (0, len(rawDistList)):
+	    distanceList[i] = rawDistList[i]*math.cos(math.radians(srvo_ang))*math.cos(abs(math.radians(angleList[i])-(math.pi/2)))
+    return distanceList
+
+#height of obstacles by angle
+
+def raw2height(heightList, rawDistList, srvo_ang):
+	  for i in range (0, len(rawDistList)):
+	   	heightList[i] = rawDistList[i]*math.sin(math.radians(srvo_ang))
+    return heightList
