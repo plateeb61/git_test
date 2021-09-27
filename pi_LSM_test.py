bf2ebb6573717c8e28bb65e4bd8ffaea52379fc8
plateeb61 <84173786+plateeb61@users.noreply.github.com>
2021-09-28 07:32:52 +0900

    Update pi_LSM_test.py

--- pi_LSM_test.py
+++ pi_LSM_test.py
@@ -5,8 +5,7 @@ import math
 # Least Square Method 
 # inliers들로 구성된 기준식 하나 구하기 (=a, b, c 구하기)
 # inliersList는 RANSAC이 return한 maxInliers 
-# 이때 maxInliers의 x값이 angle이 아니라 a*cos(angle)값인 찐 x가 넘어와야 하는데...
-# inliersList = [[x1, y1], [x2, y2], ...]
+# inliersList = [angle, distance]
 
 
 J=np.empty((0,3), float) # Jacobian matrix (mx3)
@@ -20,27 +19,30 @@ while 1:
 
         # 현재의 X 추정값 (a,b,c 추정값)에 대해 J, F를 계산함
     for i in inliersList:
-        x=param[0]*math.cos(i[0]) # x value of i번째 inliersList
+        x=param[0]*math.cos(i[0]) # angle of i번째 inliersList to x value
         y=i[1] # y value of i번째 inliersList
-        
-        Ja=(X[0]**2 - X[1]*x)/(X[0]**2*math.sqrt(1 - x**2/X[0]**2))
-        Jb=-math.acos(x/X[0])         
+        a=X[0,0]
+        b=X[1,0]
+        c=X[2,0]
+
+        Ja=(a**2 - b*x)/(a**2*math.sqrt(1 - x**2/a**2))
+        Jb=-math.acos(x/a)         
         J = np.append(J, np.array([[Ja, Jb, 1]]), axis=0)
 
-        fi=X[0]*math.sin(math.acos(x/X[0]))-X[1]*math.acos(x/X[0])+X[2]-y
+        fi=a*math.sin(math.acos(x/a))-b*math.acos(x/a)+c-y
         F = np.append(F, np.array([[fi]]), axis=0)
-        
+
         # P 계산, X 업데이트
-    print(J)
-    prinf(F)
 
     P=np.linalg.inv(J.T@J)@J.T@F # @ : 행렬곱
-    bX=X # 업데이트 전 X 
+    bX=X # 업데이트 전 X
+
     X=X-P # X 업데이트
-    print(X)
+
         # 역행렬은 정방 행렬 (nxn)에서만 정의됨. J는 정방 행렬 될 수 없음. 따라서 역행렬이 존재하지 않음 
-    if X==bX-np.linalg.inv(J.T@J)@J.T@F: 
+    if all(abs(X-bX)<0.5): 
         break
+
 
 
 
